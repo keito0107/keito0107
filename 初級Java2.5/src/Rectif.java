db52b33613a37426b925ab6a1b8fa540e3f8ea63
金澤圭人 <keitodayo.@0107.icloud.com>
2024-05-20 11:46:45 +0900

    確認問題4.4やりました。

---
+++ 初級Java2.5/src/Rectif.java
@@ -0,0 +1,23 @@
+
+public class Rectif extends MyFrame {
+	public void run() {
+		int x=30;
+		setColor(0,128,0);
+		int i;
+		for(i=0; i<10; i++) {
+			
+				fillRect(x,x,10,100);
+			x+=30;
+		
+		}
+	}
+		
+		
+				
+		
+		
+		
+		
+	}
+
+
---
+++ 初級Java2.5/src/Start.java
@@ -0,0 +1,14 @@
+
+public class Start {
+
+	public static void main(String[] args) {
+		System.out.println("プログラムを実行します");
+		MyFrame frame1=new Rectif();
+	
+		
+		
+	
+
+	}
+
+}
