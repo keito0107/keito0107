c6e30c1f74b7c0f3ed5768c1eeb5ccadd354ff9a
金澤圭人 <keitodayo.@0107.icloud.com>
2024-05-21 10:42:12 +0900

    4.3もやりました。

--- 初級Java2.5/src/Rectif.java
+++ 初級Java2.5/src/Rectif.java
@@ -2,14 +2,14 @@
 public class Rectif extends MyFrame {
 	public void run() {
 		int x=30;
-		setColor(0,128,0);
+		setColor(128,128,128);
 		int i;
-		int y=30;
+		int y=0;
 		for(i=0; i<10; i++) {
-			
-				fillRect(x,100,10,y);
+			setColor(y,y,y);
+			fillRect(x,100,10,50);
 			x+=30;
-			y+=30;
+			y+=12.8;
 			
 		
 		}
