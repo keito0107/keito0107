bfa936e120879a446a5d3c005ff638ae0df21050
金澤圭人 <keitodayo.@0107.icloud.com>
2024-05-27 11:39:49 +0900

    5の2やりました

--- 初級Java2.5/src/Animation.java
+++ 初級Java2.5/src/Animation.java
@@ -1,16 +1,32 @@
 
 public class Animation extends MyFrame{
 	public void run() {
-		int y=250;
-		int x=100;
-		while(y>=20) {
+		int y=100;
+		int x=30;
+		while(y<=200) {
 			clear();
 			setColor(0,128,0);
 			fillRect(x,y,10,100);
+			x+=5;
+			y+=5;
+			sleep(0.1);
+		}
+		while(x>=30) {
+			clear();
+			setColor(0,128,0);
+			fillRect(x,y,10,100);
+			x-=5;
+			sleep(0.1);
+		}
+		while(y>=100) {
+			clear();
+			setColor(0,128,0);
+			fillRect(x,y,10,100);
+			x+=5;
 			y-=5;
 			sleep(0.1);
 		}
-		while(x<=200) {
+		while(x<=30) {
 			clear();
 			setColor(0,128,0);
 			fillRect(x,y,10,100);
@@ -19,5 +35,6 @@ public class Animation extends MyFrame{
 		}	
 		}
 	}
+	
 		
 
